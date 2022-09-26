e452eaba98deb718cbd538d309e954558f48269b
Erik Russell <erikrussells@gmail.com>
2022-09-26 13:00:28 -0400

    commit tree 1ab126b6f377df2a66c725b4ce861ec8fe8ccaa9

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   const url = "https://anapioficeandfire.com/api/books"
+//   return fetch(url)
+//   .then((resp) => resp.json())
+//   .then((data) => renderBooks(data))
+// }
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   })
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
+
+// // function fetchBooks() {
+// //   const mainUrl = "https://anapioficeandfire.com/api/books";
+// //   return fetch(mainUrl)
+// // }
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+fetch("https://anapioficeandfire.com/api/books")
+.then(res => res.json())
+.then(data => renderBooks(data))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
+const renderBooks = (books) => {
+  const main = document.querySelector("main")
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement("h2")
+    h2.innerHTML = book.name
+    main.append(h2)
+  })
 }
-
+renderBooks()
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
+
+
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main')
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = book.name
+//     main.appendChild(h2)
+//   })
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
